af3a02fdad33a024777c2d69ddb7c85614474530
taillefumier <hugo.taillefumier@uca.fr>
2021-07-15 15:40:17 +0200

    Admin can delete every import if they have .etu

--- src/Repository/ImportedDataRepository.php
+++ src/Repository/ImportedDataRepository.php
@@ -94,39 +94,48 @@ class ImportedDataRepository extends ServiceEntityRepository
 			->getResult();
 	}
 
-	public function findRn(ImportedData $data, string $username): ?ImportedData
+	public function findRn(ImportedData $data, string $username, bool $admin = false): ?ImportedData
 	{
-		return $this->createQueryBuilder('i')
+		$query = $this->createQueryBuilder('i')
 			->where('i.semestre = :semestre')
 			->andWhere('i.session = :session')
 			->andWhere('i.libelle_form is not null')
-			->andWhere('i.username = :username')
 			->andWhere('i.etu_filename = :etu')
 			->andWhere('i.year = :year')
-			->setParameter('username', $username)
 			->setParameter('semestre', $data->getSemestre())
 			->setParameter('session', $data->getSession())
 			->setParameter('etu', $data->getEtu()->getClientOriginalName())
-			->setParameter('year', $data->getYear() . '-' . (substr($data->getYear(), 2, 2) + 1))
+			->setParameter('year', $data->getYear() . '-' . (substr($data->getYear(), 2, 2) + 1));
+
+		if (!$admin) {
+			$query->andWhere('i.username = :username')
+				->setParameter('username', $username);
+		}
+
+		return $query
 			->setMaxResults(1)
 			->getQuery()
 			->getOneOrNullResult();
 	}
 
-	public function findAttest(ImportedData $data, string $username): ?ImportedData
+	public function findAttest(ImportedData $data, string $username, bool $admin = false): ?ImportedData
 	{
-		return $this->createQueryBuilder('i')
+		$query = $this->createQueryBuilder('i')
 			->where('i.semestre IS NULL')
 			->andWhere('i.session IS NULL')
 			->andWhere('i.libelle_form IS NULL')
-			->andWhere('i.username = :username')
 			->andWhere('i.etu_filename = :etu')
 			->andWhere('i.year = :year')
-			->setParameter('username', $username)
 			->setParameter('etu', $data->getEtu()->getClientOriginalName())
-			->setParameter('year', $data->getYear() . '-' . (substr($data->getYear(), 2, 2) + 1))
-			->setMaxResults(1)
-			->getQuery()
-			->getOneOrNullResult();
+			->setParameter('year', $data->getYear() . '-' . (substr($data->getYear(), 2, 2) + 1));
+
+			if (!$admin) {
+				$query->andWhere('i.username = :username')
+					->setParameter('username', $username);
+			}
+
+			return $query->setMaxResults(1)
+				->getQuery()
+				->getOneOrNullResult();
 	}
 }
