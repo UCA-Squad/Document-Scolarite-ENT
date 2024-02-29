e6941434091083a47493522a9d64fcbe808b1a7c
hutaille <hugo.taillefumier@uca.fr>
2024-02-29 11:08:16 +0100

    update  symfony/framework-bundle recipe

--- public/index.php
+++ public/index.php
@@ -4,26 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-//<<<<<<< ours
-//if ($_SERVER['APP_DEBUG']) {
-//    umask(0000);
-//
-//    Debug::enable();
-//}
-//
-//if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-//    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
-//}
-//
-//if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-//    Request::setTrustedHosts([$trustedHosts]);
-//}
-//
-//$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-//$request = Request::createFromGlobals();
-//$response = $kernel->handle($request);
-//$response->send();
-//$kernel->terminate($request, $response);
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
