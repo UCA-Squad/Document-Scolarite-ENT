05221d54f3f362fb8054b82400154b1030f5ea52
hutaille <hugo.taillefumier@uca.fr>
2024-10-17 16:27:27 +0200

    info menu ent pour unauthorized

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -4,6 +4,9 @@
 namespace App\Security;
 
 
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -12,12 +15,29 @@ use Twig\Environment;
 
 class AccessDeniedHandler implements AccessDeniedHandlerInterface
 {
-    public function __construct(private Environment $twig)
+    public function __construct(private Environment           $twig, private Security $security, private AesCipher $aes,
+                                private ParameterBagInterface $params, private LoggerInterface $logger)
     {
     }
 
     public function handle(Request $request, AccessDeniedException $accessDeniedException): Response
     {
-        return new Response($this->twig->render('public/access_denied.html.twig'));
+        $url_login = $this->params->get('menu_url_login');
+        $url_logout = $this->params->get('menu_url_logout');
+
+        $b64Info = json_encode([
+            'username' => $this->security->getUser()->getUserIdentifier(),
+            'roles' => $this->security->getUser()->getRoles(),
+            'encryptedUsername' => $this->aes->encrypt($this->security->getUser()->getUserIdentifier()),
+            'numero' => $this->security->getUser()->getNumero(),
+            'url_login' => $url_login,
+            'url_logout' => $url_logout,
+        ]);
+
+        $this->logger->info('Access denied for user ' . $this->security->getUser()->getUserIdentifier());
+
+        return new Response($this->twig->render('public/access_denied.html.twig', [
+            'b64Info' => base64_encode($b64Info),
+        ]));
     }
 }
